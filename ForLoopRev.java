7bd5ce1f3413d16b2bd85892b6e96859d472dd57
OranMcKenna <oranmcken02@gmail.com>
2026-07-14 15:50:38 +0100

    Create ForLoopRev.java

---
+++ ForLoopRev.java
@@ -0,0 +1,42 @@
+public class ForLoopRev {
+    
+    public static void main(String[] args){
+
+        //for (initial value; test condition; modification to value)
+        for (int i = 0; i < 5; i++){ //print i if less than 5, then add 1
+            System.out.println(i);
+        }
+
+        int[] myNumbers = {10,20,30,40,50}; //declare and initialize array
+
+        for (int i = 0; i <myNumbers.length; i++){
+            System.out.print(myNumbers[i] + " ");
+        }
+
+        System.out.print("\n"); //new line
+
+        int[] myAr = {100,200,300,400,500}; //declare and initialize array
+
+        for (int a = 0; a < myAr.length; a++){
+            System.out.print(myAr[a] + " ");
+        }
+
+        System.out.print("\n"); //new line
+
+        for (int item : myAr)
+        System.out.println(item);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
