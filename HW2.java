6b85d9b3c957427edbf459c7311905e76c43e79d
OranMcKenna <oranmcken02@gmail.com>
2026-07-14 16:09:12 +0100

    Create HW2.java

---
+++ HW2.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class HW2 {
+    public static void main(String[] args) {
+
+        // System.out.println("Hello\tWorld"); //backslash t is tab
+        // System.out.println("Hello\nWorld"); //backslash n is new line
+        // System.out.println("Hello\\World"); //backslash backslash is single backslash
+        // System.out.println("Hello\"World\""); //backslash double quote is double quote
+
+        System.out.printf("\nThe answer for %.3f divided by %d is %.2f.", 5.45, 3, 5.45/3);
+        System.out.printf("\n%,d", 12345);
+
+
+    }
+}
